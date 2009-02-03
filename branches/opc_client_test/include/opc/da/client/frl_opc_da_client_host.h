381d49b4da23ff74f54b896b8064c50aa1c51934
serg.baburin <serg.baburin@daf5c975-553e-0410-8cda-3135a9dfd8f3>
2009-02-03 21:11:17 +0000

    Added scope guard in methods of the class Host; Added method RemoveConnection(..);

--- branches/opc_client_test/include/opc/da/client/frl_opc_da_client_host.h
+++ branches/opc_client_test/include/opc/da/client/frl_opc_da_client_host.h
@@ -3,6 +3,7 @@
 #include "frl_platform.h"
 #if( FRL_PLATFORM ==FRL_PLATFORM_WIN32 )
 #include <map>
+#include <boost/thread.hpp>
 #include "opc/da/client/frl_opc_da_client_server_connection.h"
 
 namespace frl{ namespace opc{ namespace da{ namespace client{
@@ -14,14 +15,18 @@ private:
 	typedef std::pair< String, ServerConnectionPtr > ConnectionListElem;
 	typedef std::map< String, ServerConnectionPtr > ConnectionList;
 	ConnectionList connection_list;
+	boost::mutex scope_guard;
+
 public:
 	FRL_EXCEPTION_CLASS( ConnectionAlreadyAdded );
 	FRL_EXCEPTION_CLASS( ConnectionNotFound );
 
 	Host( const String& name_ );
-	String getName();
+	~Host();
+	const String& getName();
 	ServerConnectionPtr addConnection( const String& server_id );
 	ServerConnectionPtr getConnection( const String& server_id );
+	void RemoveConnection( const String& server_id );
 }; // class Host
 
 typedef boost::shared_ptr< Host > HostPtr;
--- branches/opc_client_test/src/opc/da/client/frl_opc_da_client_host.cpp
+++ branches/opc_client_test/src/opc/da/client/frl_opc_da_client_host.cpp
@@ -1,5 +1,6 @@
 #include "opc/da/client/frl_opc_da_client_host.h"
 #if( FRL_PLATFORM == FRL_PLATFORM_WIN32 )
+#include <boost/foreach.hpp>
 #include "opc/da/client/frl_opc_da_client_server_connection.h"
 
 namespace frl{ namespace opc{ namespace da{ namespace client{
@@ -7,16 +8,24 @@ namespace frl{ namespace opc{ namespace da{ namespace client{
 Host::Host( const String& name_ )
 : name( name_ )
 {
-
 }
 
-frl::String Host::getName()
+Host::~Host()
+{
+	boost::mutex::scoped_lock guard( scope_guard );
+	BOOST_FOREACH( ConnectionListElem cn, connection_list )
+		cn.second->disconnect();
+}
+
+const String& Host::getName()
 {
 	return name;
 }
 
 ServerConnectionPtr Host::addConnection( const String& server_id )
 {
+	FRL_EXCEPT_GUARD();
+	boost::mutex::scoped_lock guard( scope_guard );
 	ConnectionList::iterator it = connection_list.find( server_id );
 	if( it != connection_list.end() )
 		FRL_THROW_S_CLASS( ConnectionAlreadyAdded );
@@ -27,10 +36,23 @@ ServerConnectionPtr Host::addConnection( const String& server_id )
 
 ServerConnectionPtr Host::getConnection( const String& server_id )
 {
+	FRL_EXCEPT_GUARD();
+	boost::mutex::scoped_lock guard( scope_guard );
 	ConnectionList::iterator it = connection_list.find( server_id );
 	if( it == connection_list.end() )
 		FRL_THROW_S_CLASS( ConnectionNotFound );
 	return it->second;
+}
+
+void Host::RemoveConnection( const String& server_id )
+{
+	FRL_EXCEPT_GUARD();
+	boost::mutex::scoped_lock guard( scope_guard );
+	ConnectionList::iterator it = connection_list.find( server_id );
+	if( it == connection_list.end() )
+		FRL_THROW_S_CLASS( ConnectionNotFound );
+	it->second->disconnect();
+	connection_list.erase( it );
 }
 
 } // namespace client
